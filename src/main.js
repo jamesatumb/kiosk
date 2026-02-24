8635cdb7e53b3d14f93441e3b3552b593ab1d2a3
James M <134722578+jamesatumb@users.noreply.github.com>
2026-02-24 13:11:24 -0600

    initial commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+
+function setup() {
+
+  Alpine.store('model', {
+    currentPage: 'home', // 'home', 'service'
+    currentLanguage: 'english',
+    dialNumber1: '8168608855',
+    dialNumber2: '8607777',
+    services: [],
+
+    init() {
+      const params = new URLSearchParams(location.search);
+      if (params.has('number')) {
+        this.dialNumber = params.get('number');
+      }
+      this.services = [
+        { url: this.dialNumber1, name: 'Tech Support' },
+        { url: this.dialNumber2, name: 'Help Desk' },
+      ];
+    },
+    get page() {
+      return this.currentPage;
+    },
+    set page(nextPage) {
+      this.currentPage = nextPage;
+    },
+    currentLanguage: 'english',
+    languages: ['english', 'norwegian'],
+    get language() {
+      return this.currentLanguage;
+    },
+    set language(current) {
+      this.currentLanguage = current;
+    },
+  });
+
+}
+
+document.addEventListener('alpine:init', setup);
