9d4c57ffd0c3f02309091be54c22a6985e3d2f71
James M <134722578+jamesatumb@users.noreply.github.com>
2026-03-02 11:20:45 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@
 
 async function setup() {
   
-  import xapi from 'xapi';
-  await xapi.Config.UserInterface.Kiosk.Mode.set('On');
-  await xapi.Config.UserInterface.Kiosk.Keypad.set('On');
-
+  // import xapi from 'xapi';
+  // await xapi.Config.UserInterface.Kiosk.Mode.set('On');
+  // await xapi.Config.UserInterface.Kiosk.Keypad.set('On');
+  xapi.Config.UserInterface.Kiosk.Mode
+      .set(On);
+  xapi.Config.UserInterface.Keypad.Keypad
+      .set(On);
 
   Alpine.store('model', {
     currentPage: 'home', // 'home', 'service'
